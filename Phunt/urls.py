2ca977bbeb96300b05ec9582d68ebeb798437490
MdArifulislam21 <sabuj19977@gmail.com>
2020-03-03 03:15:59 +0600

    Authentication

--- Phunt/urls.py
+++ Phunt/urls.py
@@ -2,12 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from products.views import home
-import accounts
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name= 'home'),
-    path('account/', include('accounts.urls')),
-]
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
